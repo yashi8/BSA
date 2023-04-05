ac38b5e2f53487962d9c78142757400427f942de
petrichoraroma <yarasights@gmail.com>
2023-04-05 21:33:04 +0530

    Updates to Vendor Fragments

--- app/src/main/java/com/yashishu/bsa/ui/vendor/ViewAccountsFragment.kt
+++ app/src/main/java/com/yashishu/bsa/ui/vendor/ViewAccountsFragment.kt
@@ -1,10 +1,11 @@
-package com.yashishu.bsa
+package com.yashishu.bsa.ui.vendor
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.yashishu.bsa.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
